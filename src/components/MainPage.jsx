1401c18db61bd50421b2db3cc2cb94dad87d650c
Raman Davidovich <raman.davidovich@gmail.com>
2025-01-21 19:31:42 +0300

    feat: implemented fetching images

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,7 +1,46 @@
+import { useEffect, useState } from "react";
+
 const MainPage = () => {
+  const [catImages, setCatImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCatImages = async () => {
+      try {
+        setLoading(true);
+
+        const response = await fetch(
+          "https://api.thecatapi.com/v1/images/search?limit=10"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch cat images.");
+        }
+
+        const data = await response.json();
+        setCatImages(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCatImages();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
-    <p>Main page</p>
-  )
-}
+    <div>
+      <div class="images">
+        {catImages.map((cat) => (
+          <img key={cat.id} src={cat.url} alt="A cute cat" class="image" />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default MainPage;
